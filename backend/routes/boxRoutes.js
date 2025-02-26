c8e3cb5d5198dcfd2fb1eea509ffa8565d7443e3
Venura Kaushan <86141130+VenuraKaushan@users.noreply.github.com>
2025-02-26 16:14:51 +0530

    Update boxRoutes.js

--- backend/routes/boxRoutes.js
+++ backend/routes/boxRoutes.js
@@ -4,8 +4,7 @@ import { getBoxes } from "../controllers/boxController.js";
 const router = express.Router();
 
 router.get("/boxes", getBoxes);
-// router.post("/tasks", add);
-// router.delete("/tasks/:id", deleteTask);
+
 
 
 export default router;
